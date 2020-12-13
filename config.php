8c7e4b6d3bfa87197a8614127100ac848358652d
AbdElRhman ElAmin <abdel-Rhman.El-Amin@ibm.com>
2020-12-13 14:46:15 +0200

    update database credintials

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'Amin_1234');
-   define('DB_DATABASE', 'todoDB');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+$db = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 
    if($db === false){
       die("ERROR: Could not connect. " . mysqli_connect_error());
